1dab2db3fc46bea4c8cf5509aecfd2714bbb1f88
adrikahoque <149006165+adrikahoque@users.noreply.github.com>
2023-11-22 21:01:45 -0500

    Updated code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
